a3affd78644c70b52810b737311dd23719abbaca
warlock <h.kwiatkowski@hotmail.com>
2021-12-12 09:37:55 +0100

    first app

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from studybud import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('room/', views.room, name="room"),
+    path('room/<str:pk>/', views.room, name="room"),
 ]
